c3220f58309ad9b72639532605ea75d8171aaf15
Zhenyu.Zhang <1923791@qq.com>
2016-10-10 16:12:44 +0800

    Initial commit commit test !

---
+++ WEB-INF/src/lee/GetApplication.java
@@ -0,0 +1,36 @@
+package lee;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import java.io.*;
+
+/**
+ * Description:
+ * <br/>???: <a href="http://www.crazyit.org">???Java????</a>
+ * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author  Yeeku.H.Lee kongyeeku@163.com
+ * @version  1.0
+ */
+@WebServlet(name="get-application",
+	urlPatterns={"/get-application"})
+public class GetApplication extends HttpServlet
+{
+	public void service(HttpServletRequest request,
+		HttpServletResponse response)throws IOException
+	{
+		response.setContentType("text/html;charset=gb2312");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>");
+		out.println("????application");
+		out.println("</title></head><body>");
+		ServletContext sc = getServletConfig().getServletContext();
+		out.print("application?§Ö????counter??:");
+		out.println(sc.getAttribute("counter"));
+		out.println("</body></html>");
+	}
+}
